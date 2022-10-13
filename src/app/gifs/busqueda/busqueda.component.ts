1dbfe6206aca107e2257e196955385bdafe46b63
jcarloshg <carlosj12336@gmail.com>
2022-10-13 11:22:16 -0500

    CREATE [ViewChild] into [BusquedaComponent]

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
 
-  constructor() { }
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  buscar(keyword: string) {
+    const value = this.txtBuscar.nativeElement.value;
+    console.log(`[value] -> `, value);
+    this.txtBuscar.nativeElement.value = '';
   }
 
+  // buscar(event: KeyboardEvent) {
+  //   console.log('====================================');
+  //   console.log(event.key);
+  //   console.log('====================================');
+  // }
 }
--- src/app/gifs/resultados/resultados.component.ts
+++ src/app/gifs/resultados/resultados.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-resultados',
   templateUrl: './resultados.component.html',
   styleUrls: ['./resultados.component.css']
 })
-export class ResultadosComponent implements OnInit {
+export class ResultadosComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
